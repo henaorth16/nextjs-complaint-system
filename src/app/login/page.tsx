1a56417530aaa9f7be04bea0b061ea047600427b
bisratolera <leultolera@gmail.com>
2024-06-28 22:10:58 +0300

    login page ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, FormEvent } from 'react';
-import { useRouter } from 'next/navigation'; // Use 'next/navigation' instead of 'next/router'
+import { useRouter } from 'next/navigation'; 
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
@@ -11,37 +11,44 @@ const LoginPage = () => {
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
-    console.log("hello")
-  }
+    console.log("hello");
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='username'>username:</label>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            id='username'
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            id='password'
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      {message && <p>{message}</p>}
+    <div className='flex justify-center items-center h-screen'>
+      <div className=' p-8 rounded-lg shadow-md w-full max-w-sm'>
+        <h1 className='text-2xl mb-6 text-center'>MLITC Login</h1>
+        <form onSubmit={handleSubmit} className='space-y-4'>
+          <div className='flex flex-col'>
+            <label htmlFor='username' className='mb-2'>Username:</label>
+            <input
+              type='text'
+              name='username'
+              id='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className='p-2 border rounded-lg'
+            />
+          </div>
+          <div className='flex flex-col'>
+            <label htmlFor='password' className='mb-2'>Password:</label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className='p-2 border rounded-lg'
+            />
+          </div>
+          <button type='submit' className='w-full p-2 bg-primary text-black  rounded-lg hover:bg-secondary'>
+            Login
+          </button>
+        </form>
+        {message && <p className='text-red-500 mt-4 text-center'>{message}</p>}
+      </div>
     </div>
   );
 };
