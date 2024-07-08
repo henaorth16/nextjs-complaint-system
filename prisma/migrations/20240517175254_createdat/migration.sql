caff0290b658eaee33dd6aab39bc7155fa8e9a2f
henaorth16 <emyayehenok@gmail.com>
2024-07-09 00:15:18 +0300

    Switch db to postgres

--- prisma/migrations/20240517175254_createdat/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `createdAt` on the `faq` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `faq` DROP COLUMN `createdAt`;
